7f7fd3a28c9f987fe8ed1810a6a66d7a3a40476c
Jad alKhawand <jkh42005@gmail.com>
2024-04-07 19:35:13 +0300

    shopping cart function

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,23 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import React, { useCallback } from "react";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@assets/data/products";
 import { Image, StyleSheet, Pressable } from "react-native";
 import { defaultPizzaImage } from "@/components/ProductListItem";
 import { useState } from "react";
 import Button from "@/components/Button";
+import { useCart } from "@/providers/CartProvider";
+import { PizzaSize } from "@/types";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
-
-  const [selectedSize, setSelectedSize] = useState("M");
+  const {addItem} = useCart()
+ const router = useRouter()
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
 
   const product = products.find((p) => p.id.toString() === id);
 
   const addToCart = () => {
-    console.warn("Adding to cart, size: ",selectedSize);
+    if(!product)
+      return
+    addItem(product, selectedSize)
+    router.push("/cart")
   };
 
   if (!product) return <Text> Product Not Found</Text>;
